a039c0cd5f56b947b5960d73ea23b86ede6e5008
CharkaouiSalwa <salwacharkaoui@gmail.com>
2023-11-10 13:25:44 +0100

    update config.yml

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = env('SECRET_KEY', default=get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 sentry_sdk.init(
     dsn=env('SENTRY_DSN'),
