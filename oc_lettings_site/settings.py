90efd49e75e20238e850ee6b0a0ca91d8ca14f2a
CharkaouiSalwa <salwacharkaoui@gmail.com>
2023-11-13 12:57:08 +0100

    add flake8

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -5,7 +5,6 @@ import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 from sentry_sdk.integrations.logging import LoggingIntegration
 import environ
-from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).resolve().parent.parent
