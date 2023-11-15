e0ff764b0d0a3dd56faf202c2a8760633992bedb
CharkaouiSalwa <salwacharkaoui@gmail.com>
2023-11-15 11:21:24 +0100

    flake8

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -7,6 +7,7 @@ from sentry_sdk.integrations.logging import LoggingIntegration
 import environ
 from django.core.management.utils import get_random_secret_key
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -14,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(DEBUG=(bool, False))
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 DEBUG = env('DEBUG')
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY', default=get_random_secret_key())
 
 
 # Quick-start development settings - unsuitable for production
